588c8b753ed78847598f51d1b021114443812e0f
Sławomir Wójcik <swojcik1996@gmail.com>
2019-07-30 18:22:41 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@
 
 	// Typing Animation (Typed.js)
 	$('#element').typed({
-	    strings: ["Test Development Engineer","in Scalepoint Technologies."],
+	    strings: ["Test Development Engineer","in Scalepoint Technologies"],
 	    typeSpeed: -50,
 	    loop: true,
 	    startDelay: 500,
